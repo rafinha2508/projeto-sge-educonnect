57f883559cedfefb53070161f781c5976eab132f
rafinha2508 <rafaekely51@gmail.com>
2025-12-06 03:25:46 -0300

    Implementa classe Avaliacao e ajusta métodos em Aluno

---
+++ src/Avaliacao.java
@@ -0,0 +1,28 @@
+public class Avaliacao {
+    private double nota;
+    private String descricao;
+
+    public void setNota(double valor) {
+        if (valor >= 0 && valor <= 10) {
+            this.nota = valor;
+        }  else {
+            System.out.println("O valor digitado deve ser entre 0 e 10!");
+        }
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+
+
+}
+
